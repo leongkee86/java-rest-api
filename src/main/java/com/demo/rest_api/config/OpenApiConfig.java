5a72e5ed5af5a9322d9606358c3d70612e30909c
Lim Leong Kee <leongkee86@gmail.com>
2025-08-20 04:10:17 +0800

    - Updated the description displayed at the top part of the Swagger UI page.

--- src/main/java/com/demo/rest_api/config/OpenApiConfig.java
+++ src/main/java/com/demo/rest_api/config/OpenApiConfig.java
@@ -22,7 +22,37 @@ import java.util.TreeMap;
     info = @Info(
         title = "Server APIs (Java + MongoDB)",
         version = "0.0.2",
-        description = "Hi. I am Lim Leong Kee.<br>I have developed the entire set of server-side APIs using Java, with MongoDB as the underlying database. These APIs are fully documented and available for exploration and interaction via this page.<br>I would sincerely appreciate any feedback, suggestions or improvements you might have. Thank you for taking the time to review my work.",
+        description = """
+            Hi. I am Lim Leong Kee.
+            
+            I have developed the entire set of server-side APIs using **Java**, with **MongoDB** as the underlying database. These APIs are fully documented and available for exploration and interaction via this page.
+            
+            You can register for an account, then log in to:
+            
+              - Play games:
+                
+                1. Guess Number
+                
+                2. Arrange Numbers
+                
+                3. Rock Paper Scissors (practice and challenge modes)
+                
+              - View your game profile or another user's game profile
+                
+              - Check the leaderboard
+                
+            > All API endpoints are interactive and can be tested directly from this page!
+                
+            I also customized this Swagger UI to include the following features:
+            
+            1. **Automatic JWT Handling:** Captures and injects the JWT token after a successful login to enable seamless access to protected endpoints.
+            
+            2. **Logout Behavior:** Clears the authorization token on logout to disable access to protected endpoints until you log in again.
+            
+            3. **Custom Operation Sorting:** Implements a custom sorting logic for API operations based on a predefined order, making the API navigation more intuitive.
+        
+            I would sincerely appreciate any feedback, suggestions, or improvements you might have. Thank you for taking the time to review my work.
+            """,
         contact = @Contact(
             name = "Lim Leong Kee",
             email = "leongkee86@gmail.com"
